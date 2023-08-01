4f2156b9ba54299f353d0f6687a73d6099ca8b0f
NickLu <word4205@gmail.com>
2023-08-01 21:21:18 +0800

    新增_HASdata

---
+++ LMSweb/Migrations/20230801131817_fix_OptionId_data_type.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMSweb.Migrations
+{
+    /// <inheritdoc />
+    public partial class fix_OptionId_data_type : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "OptionID",
+                table: "Options",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "OptionID",
+                table: "Options",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+        }
+    }
+}
