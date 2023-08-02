9b39d5546b7174af2587f7fc0f1398ccc4286d1d
NickLu <word4205@gmail.com>
2023-08-02 23:40:41 +0800

    新增_問卷頁面

---
+++ LMSweb/Controllers/Questionnaire/QuestionnaireController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LMSweb.Controllers.Questionnaire
+{
+    public class QuestionnaireController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
