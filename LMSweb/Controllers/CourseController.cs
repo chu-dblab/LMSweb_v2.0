daa3fdd86f621ef5d3240efb506f33db3daafe2f
林廣學 <tomkhlin@g.chu.edu.tw>
2023-08-03 15:48:25 +0800

    修正_api無法處理request body中的資料 新增_CourseController 加入_CORS支援(for 測試，正式上線時需註解掉)

---
+++ LMSweb/Controllers/CourseController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMSweb.Controllers
+{
+    [Authorize]
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
