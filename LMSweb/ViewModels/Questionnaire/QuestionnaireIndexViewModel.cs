2ecd8ee6cbef07a89157b9168f0c285ac4498543
NickLu <word4205@gmail.com>
2023-08-07 23:50:35 +0800

    新增_提交答案

---
+++ LMSweb/ViewModels/Questionnaire/QuestionnaireIndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LMSweb.ViewModels.Questionnaire
+{
+    public class QuestionnaireIndexViewModel
+    {
+        public string MissionName { get; set; }
+        public string TaskType { get; set; }
+        public string TaskSteps { get; set; }
+    }
+}
