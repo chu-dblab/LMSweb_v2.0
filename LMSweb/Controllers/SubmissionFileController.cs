e91646b3484d87eb652f3af58c26fd5bc168ecb8
NickLu <word4205@gmail.com>
2023-08-09 13:48:19 +0800

    新增_上傳繳交圖片

--- LMSweb/Controllers/SubmissionFileController.cs
+++ LMSweb/Controllers/SubmissionFileController.cs
@@ -1,4 +1,6 @@
 ﻿using LMSweb.Data;
+using LMSweb.Models;
+using LMSweb.Services;
 using LMSweb.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,19 +10,68 @@ namespace LMSweb.Controllers
     {
         private readonly ILogger<StudentController> _logger;
         private readonly LMSContext _context;
+        private readonly FileUploadService _fileUploadService;
 
-        public SubmissionFileController(ILogger<StudentController> logger, LMSContext context)
+        public SubmissionFileController(ILogger<StudentController> logger, LMSContext context, FileUploadService fileUploadService)
         {
             _logger = logger;
             _context = context;
+            _fileUploadService = fileUploadService;
         }
 
 
-        public IActionResult Index(string mid)
+        public IActionResult Index(string mid, string type)
         {
             var vm = new SubmissionFileViewModel();
+            vm.type = type;
+            var UID = User.Claims.FirstOrDefault(x => x.Type == "UID").Value;   //抓出當初記載Claims陣列中的TID
 
-            return View();
+            var GroupId = _context.Students.Where(x => x.StudentId == UID).FirstOrDefault().GroupId;
+            var MisstionId = _context.Executions.Where(x => x.GroupId == GroupId).FirstOrDefault().MissionId;
+            var misstion = _context.Missions.Find(mid);
+
+            vm.CourseName = _context.Courses.FirstOrDefault(x => x.Cid == _context.Students.Where(x => x.StudentId == UID).FirstOrDefault().CourseId).Cname;
+            vm.MisstionId = MisstionId;
+            vm.MisstionName = _context.Missions.FirstOrDefault(x => x.CourseId == _context.Students.Where(x => x.StudentId == UID).FirstOrDefault().CourseId).Mname;
+            vm.EndDate = _context.Missions.FirstOrDefault(x => x.CourseId == _context.Students.Where(x => x.StudentId == UID).FirstOrDefault().CourseId).EndDate;
+            
+            if (UID == null || misstion == null) { return NotFound(); }
+
+            var _ExecutionContent = _context.ExecutionContents.Where(x => x.GroupId == GroupId && x.MissionId == MisstionId && x.Type == type).FirstOrDefault();
+            
+            if(_ExecutionContent != null )
+            {
+                vm.Path = _ExecutionContent.Path;
+            } 
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public IActionResult Index(string mid, string type, SubmissionFileViewModel vm)
+        {
+            if (vm.Path == null) { return NotFound(); }
+             
+            var UID = User.Claims.FirstOrDefault(x => x.Type == "UID");   //抓出當初記載Claims陣列中的TID
+
+            if (UID == null) { return NotFound(); }
+
+            var GroupId = _context.Students.Where(x => x.StudentId == UID.Value).FirstOrDefault().GroupId;
+            var MisstionId = _context.Executions.Where(x => x.GroupId == GroupId).FirstOrDefault().MissionId;
+
+            var path = _fileUploadService.SaveFile(vm.formFile, "UploadImgs");
+
+            if(path == null) { return NotFound(); }
+
+            var _ExecutionContent = new ExecutionContent()
+            {
+                MissionId = MisstionId,
+                GroupId = GroupId ?? 0,
+                Path = vm.formFile.FileName ?? "no fileName",
+                Type = vm.type ?? "no type",
+            };
+
+            return View(vm);
         }
     }
 }
--- LMSweb/ViewModels/SubmissionFileViewModel.cs
+++ LMSweb/ViewModels/SubmissionFileViewModel.cs
@@ -2,5 +2,14 @@
 {
     public class SubmissionFileViewModel
     {
+        public string CourseName { get; set; }
+        public string MisstionId { get; set; }
+        public string MisstionName { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public string? type { get; set; }
+        public string? Path { get; set; }
+
+        public IFormFile? formFile { get; set; }
     }
 }
