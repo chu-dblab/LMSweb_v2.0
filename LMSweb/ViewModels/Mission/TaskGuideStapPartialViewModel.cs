b576e3a6a2ea09b85e71d57d111e02b4806c84b0
NickLu <word4205@gmail.com>
2023-08-31 10:09:01 +0800

    新增_非實驗組一鎖定網址功能

--- LMSweb/ViewModels/Mission/TaskGuideStapPartialViewModel.cs
+++ LMSweb/ViewModels/Mission/TaskGuideStapPartialViewModel.cs
@@ -6,5 +6,9 @@
         public string CurrentAction { get; set; }
         public int k { get; set; }
         public string buttonLink { get; set; }
+
+        // 判斷是否需要給 URL 連結
+        public int TestType { get; set; }
+        public bool IsLeader { get; set; }
     }
 }
