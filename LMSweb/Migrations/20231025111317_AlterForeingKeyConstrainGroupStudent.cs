a309002421abd5a75eb86f990e02b2695bed080a
林廣學 <tomkhlin@gmail.com>
2023-10-25 19:15:00 +0800

    更新_學生與組別和User之間的外部鍵刪除規則

---
+++ LMSweb/Migrations/20231025111317_AlterForeingKeyConstrainGroupStudent.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMSweb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterForeingKeyConstrainGroupStudent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Groups",
+                table: "Students");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Users",
+                table: "Students");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Groups",
+                table: "Students",
+                column: "GroupID",
+                principalTable: "Groups",
+                principalColumn: "GID",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Users",
+                table: "Students",
+                column: "StudentId",
+                principalTable: "Users",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Groups",
+                table: "Students");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Users",
+                table: "Students");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Groups",
+                table: "Students",
+                column: "GroupID",
+                principalTable: "Groups",
+                principalColumn: "GID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Users",
+                table: "Students",
+                column: "StudentId",
+                principalTable: "Users",
+                principalColumn: "ID");
+        }
+    }
+}
