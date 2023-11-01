f377fd1888ad2f89497cc03c2a4f9a19198fa417
NickLu <word4205@gmail.com>
2023-11-01 11:14:58 +0800

    分享_解決問題

---
+++ LMSweb/Controllers/LearningProcessController.cs
@@ -0,0 +1,53 @@
+﻿using LMSweb.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMSweb.Controllers
+{
+    public class LearningProcessController : Controller
+    {
+        public IActionResult Index()
+        {
+            var viewModel = new LearningProcessViewModel
+            {
+                CourseId = "C0001",
+                MissionId = "M0001",
+                TestType = 1,
+                CourseAgv = new ChartData
+                {
+                    Data = new double[] { 1, 2, 3 }
+                },
+                GroupAgv = new ChartData
+                {
+                    Data = new double[] { 2, 1, 2 }
+                },
+                Detail = new DetailData[]
+                {
+                    new DetailData
+                    {
+                        Score = new int[] { 1, 2, 3 },
+                        TestFeedback = new string[] { "你們小組程式碼寫得很好", "你們小組流程圖畫得不錯", "加油" },
+                    },
+                    new DetailData
+                    {
+                        Score = new int[] { 2, 2, 3 },
+                        TestFeedback = new string[] { "1", "2", "3" },
+                        ReTestFeedback = new string[] { "非常合理", "你們小組流程圖畫得不錯" }
+                    },
+                    new DetailData
+                    {
+                        Score = new int[] { 3, 2, 3 },
+                        TestFeedback = new string[] { "1", "2", "3" },
+                    },
+                    new DetailData
+                    {
+                        Score = new int[] { 1, 1, 3 },
+                        TestFeedback = new string[] { "1", "2", "3" },
+                        ReTestFeedback = new string[] { "1", "2", "3" }
+                    }
+                }
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ LMSweb/ViewModels/LearningProcessViewModel.cs
@@ -0,0 +1,35 @@
+﻿namespace LMSweb.ViewModels
+{
+    public class LearningProcessViewModel
+    {
+        // 返回任務介面用
+        public string CourseId { get; set; }
+        public string MissionId { get; set; }
+
+
+        // 這邊用於判斷要不要顯示切換組別模組
+        public int TestType { get; set; }
+
+        public ChartData CourseAgv { get; set; }
+        public ChartData GroupAgv { get; set; }
+
+        public DetailData[] Detail { get; set; }
+    }
+
+    public class ChartData
+    {
+        //public string[] Labels { get; set; }
+        public double[] Data { get; set; }
+    }
+
+    public class DetailData
+    {
+        // 評價
+        public int[] Score { get; set; }
+        public string[] TestFeedback { get; set; }
+
+        // -------------------------------------------
+        // 回饋
+        public string[]? ReTestFeedback { get; set; }
+    }
+}
