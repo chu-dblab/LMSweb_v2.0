fa3bbb0df42e42924068759dce07906404469954
NickLu <word4205@gmail.com>
2023-11-20 16:59:51 +0800

    修正_無法刪除

--- LMSweb/Controllers/CourseController.cs
+++ LMSweb/Controllers/CourseController.cs
@@ -148,9 +148,9 @@ namespace LMSweb.Controllers
                     foreach (var execution in executions)
                     {
                         _context.Executions.Remove(execution);
-                        _context.SaveChanges();
                     }
                 }
+                _context.SaveChanges();
 
                 // 依據 Course 刪除 mission 表中的資料
                 var missions = _context.Missions.Where(x => x.CourseId == cid);
