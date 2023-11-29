3c0d783809ec16a0cdbadfc30022fcca1a9730b7
NickLu <word4205@gmail.com>
2023-11-29 21:52:12 +0800

    修復_TeacherEva_使介面無法顯示問題

--- LMSweb/Controllers/Guide/GuideController.cs
+++ LMSweb/Controllers/Guide/GuideController.cs
@@ -162,9 +162,10 @@ namespace LMSweb.Controllers.Guide
                     }
                 }
 
-                var _TeacherEva = _context.EvaluationCoachings.Where(x => x.AUID == uid.Value && x.BUID == groupLeader && x.MissionId == mid).FirstOrDefault().Evaluation;
+                // 這邊會有問題
+                var _TeacherEva = _context.EvaluationCoachings.Where(x => x.AUID == uid.Value && x.BUID == groupLeader && x.MissionId == mid).FirstOrDefault();
 
-                if(_TeacherEva != null)
+                if(_TeacherEva.Evaluation != null)
                 {
                     _guideGroups.IsCorrect = true;
                 }
